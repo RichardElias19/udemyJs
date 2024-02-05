e4a8496abe93eab3b6034524e8568b5087ca4dda
Richard Ferreira elias <Richtheguytwo@Gmail.com>
2024-02-05 10:56:48 -0300

    desafio

--- desafio.js/D21.js
+++ desafio.js/D21.js
@@ -7,13 +7,13 @@ function indentificar( nome, idade) {
     if (idade <= 0 || idade > 120) {
         console.log(`Essa idade nao existe${idade}`)
     } else if (idade <= 10 && idade > 0) {
-        console.log(`${nome} tem ${idade} e vai pagar R$ 80,00`)
+        console.log(`${nome} tem ${idade} e vai pagar R$ 180,00`)
     } else if (idade <= 30 && idade > 10) {
-        console.log(`${nome} tem ${idade} e vai pagar R$ 50,00`)
+        console.log(`${nome} tem ${idade} e vai pagar R$ 150,00`)
     } else if(idade >= 30 && idade < 60) {
-        console.log(`${nome} tem ${idade} e vai pagar R$ 95,00`)
+        console.log(`${nome} tem ${idade} e vai pagar R$ 195,00`)
     } else if(idade > 60) {
-        console.log(`${nome} tem ${idade} e vai pagar R$ 130,00`)
+        console.log(`${nome} tem ${idade} e vai pagar R$ 230,00`)
     }
 }
 indentificar(idade = -1)
